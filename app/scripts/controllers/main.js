faa3d5cba4b423c993240e15dd5e1ac026a45596
Wojciech Krzystek <wojciech.krzystek@gmail.com>
2013-10-05 11:13:28 +0200

    sth - TO AMEND

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,11 +3,21 @@
 // TODO vucalur: rename main.js → editor.js
 
 
-function EditCtrl($scope, jsonStorageService) {
+function EditCtrl($window, $scope, jsonStorageService) {
 	$scope.script = jsonStorageService.query();
+
+	function addParam(whereToAdd, kvHolder) {
+		$window.alert(whereToAdd);
+		whereToAdd.params[kvHolder.key] = kvHolder.value;
+		kvHolder.key = kvHolder.value = '';
+	}
+
+	$scope.addSectionParam = function () {
+		addParam($scope.script.sections, $scope);
+	};
 }
 
-EditCtrl.$inject = ['$scope', 'jsonStorageService'];
+EditCtrl.$inject = ['$window', '$scope', 'jsonStorageService'];
 
 
 // TODO vucalur: make below working:
